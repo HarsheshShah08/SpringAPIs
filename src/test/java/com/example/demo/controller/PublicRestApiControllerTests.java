3f878d2b7d88694c10ed8292ffefa53c2ab0bae6
Harshesh <Harshesh@DESKTOP-06092015.mynet>
2020-04-25 10:43:42 +0100

    mockMVC test added for test the output of the API

--- src/test/java/com/example/demo/controller/PublicRestApiControllerTests.java
+++ src/test/java/com/example/demo/controller/PublicRestApiControllerTests.java
@@ -19,7 +19,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -44,7 +46,9 @@ class PublicRestApiControllerTests {
 	@Test
 	final void test() throws Exception {
 		
-		this.mockMvc.perform(get("/api/public/test1")).andExpect(status().isOk());
+		
+		
+		this.mockMvc.perform(get("/api/public/test1")).andExpect(status().isOk()).andExpect(content().string("API Test 1"));
 
 		
 	}
